c8a0b6df391aa8e0bda16b795810e616c6e2f272
Akın Haydaroğlu <gr4ndsmurf@gmail.com>
2025-05-16 00:04:43 +0300

    Unity commit

---
+++ Assets/HyperCasual-CharacterController/Scripts/CharacterMovementManager.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class CharacterMovementManager : MonoBehaviour
+{
+    [SerializeField] private Canvas inputCanvas;
+    [SerializeField] private VariableJoystick joystick;
+    [SerializeField] private CharacterController controller;
+    [SerializeField] private Animator playerAnimator;
+    
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private float rotationSpeed;
+    
+    
+    private bool isJoystick;
+    
+    void Start()
+    {
+        EnableJoystickInput();
+    }
+
+    void Update()
+    {
+        if (isJoystick)
+        {
+            // Movement
+            Vector3 movementDirection = new Vector3(joystick.Direction.x, 0.0f, joystick.Direction.y);
+            controller.SimpleMove(movementDirection * movementSpeed);
+            
+            if (movementDirection.sqrMagnitude <= 0)
+            {
+                playerAnimator.SetBool("IsRunning", false);
+                return;
+            }
+            playerAnimator.SetBool("IsRunning", true);
+            
+            // Rotation
+            Vector3 targetDirection = Vector3.RotateTowards(controller.transform.forward, movementDirection, rotationSpeed * Time.deltaTime, 0.0f);
+            controller.transform.rotation = Quaternion.LookRotation(targetDirection);
+        }
+    }
+    
+    public void EnableJoystickInput(){
+        isJoystick = true;
+        inputCanvas.gameObject.SetActive(true);
+    }
+    
+}
